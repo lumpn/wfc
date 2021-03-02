0fe95e92c5579d9b437c9d4da7fa492717a5d7a3
lumpn <lumpn@users.noreply.github.com>
2021-03-02 15:41:05 +0800

    Added math package

---
+++ Assets/Scripts/QuadDemo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class QuadDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float3x3 matrix;
+        float2x4
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
