d90f311f051b5c1d97154aad2c86cd2ee9fdc794
lumpn <lumpn@users.noreply.github.com>
2021-03-03 23:09:58 +0800

    Added bilerp

--- Assets/Scripts/QuadDemo.cs
+++ Assets/Scripts/QuadDemo.cs
@@ -1,20 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.Mathematics;
 
 public class QuadDemo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private static Vector2 Bilerp(Vector2 p00, Vector2 p01, Vector2 p10, Vector2 p11, float u, float v)
     {
-        float3x3 matrix;
-        float2x4
+        var p0 = Vector2.LerpUnclamped(p00, p01, u);
+        var p1 = Vector2.LerpUnclamped(p10, p11, u);
+        var p = Vector2.LerpUnclamped(p0, p1, v);
+        return p;
+
+        // r  =  1-u
+        // s  =  1-v
+        // p0 = (1-u) * p00 + u * p01
+        // p1 = (1-u) * p10 + u * p11
+        // p  = (1-v) * p0  + v * p1
+        //    = (1-v) * ((1-u) * p00 + u * p01) + v * ((1-u) * p10 + u * p11)
+        //    =     s * (r * p00 + u * p01) + v * (r * p10 + u * p11)
+        //    =     rs*p00 + us*p01 + rv*p10 + uv*p11
+        //    = (p00, p01, p10, p11) * (rs, us, rv, uv)T
     }
 
-    // Update is called once per frame
-    void Update()
+    private static float2 Bilerp(float2x4 quad, float2 p)
     {
-        
+        // u = p.x
+        // v = p.y
+        // r = q.x
+        // s = q.y
+        float2 q = 1 - p;
+        float4 f = new float4(q.x * q.y, p.x * q.y, q.x * p.y, p.x * p.y);
+        return math.mul(quad, f);
     }
 }
