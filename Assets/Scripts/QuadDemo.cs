0d3971a242b58f1fe2af2dea1bf1c50c29277365
lumpn <lumpn@users.noreply.github.com>
2021-03-08 23:23:30 +0800

    Added gizmo to quad demo

--- Assets/Scripts/QuadDemo.cs
+++ Assets/Scripts/QuadDemo.cs
@@ -3,6 +3,76 @@ using Unity.Mathematics;
 
 public class QuadDemo : MonoBehaviour
 {
+    [SerializeField] private Transform a, b, c, d;
+    [SerializeField, Range(0, 1)] private float u, v;
+    [SerializeField] private Transform mapped;
+
+    void Update()
+    {
+        var pos = GetBilerp(u, v);
+        mapped.localPosition = new Vector3(pos.x, 0, pos.y);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawLine(a.localPosition, b.localPosition);
+        Gizmos.DrawLine(b.localPosition, d.localPosition);
+        Gizmos.DrawLine(d.localPosition, c.localPosition);
+        Gizmos.DrawLine(c.localPosition, a.localPosition);
+
+        for (int i = 0; i <= 10; i++)
+        {
+            for (int j = 0; j <= 10; j++)
+            {
+                var gu = i * 0.1f;
+                var gv = j * 0.1f;
+                var pos = GetBilerp2(gu, gv);
+                DrawCross(new Vector3(pos.x, 0, pos.y));
+            }
+        }
+    }
+
+    private static void DrawCross(Vector3 pos)
+    {
+        var dx = new Vector3(0.01f, 0, 0);
+        var dz = new Vector3(0, 0, 0.01f);
+        Gizmos.DrawLine(pos - dx, pos + dx);
+        Gizmos.DrawLine(pos - dz, pos + dz);
+    }
+
+    private Vector2 GetBilerp(float bu, float bv)
+    {
+        var p00 = GetPosition(a);
+        var p01 = GetPosition(b);
+        var p10 = GetPosition(c);
+        var p11 = GetPosition(d);
+
+        var pos = Bilerp(p00, p01, p10, p11, bu, bv);
+        return pos;
+    }
+
+    private float2 GetBilerp2(float bu, float bv)
+    {
+        var p00 = GetPosition2(a);
+        var p01 = GetPosition2(b);
+        var p10 = GetPosition2(c);
+        var p11 = GetPosition2(d);
+
+        var quad = new float2x4(p00, p01, p10, p11);
+        return Bilerp(quad, new float2(bu, bv));
+    }
+
+    private static Vector2 GetPosition(Transform tr)
+    {
+        var pos = tr.localPosition;
+        return new Vector2(pos.x, pos.z);
+    }
+
+    private static float2 GetPosition2(Transform tr)
+    {
+        var pos = tr.localPosition;
+        return new float2(pos.x, pos.z);
+    }
 
     private static Vector2 Bilerp(Vector2 p00, Vector2 p01, Vector2 p10, Vector2 p11, float u, float v)
     {
